3c5a1d98c65078ace548c1b8421656f68b8b04c3
Victoria <victoriathorsenn@gmail.com>
2021-06-02 23:36:06 -0400

    commit tree af52e0ea4c1d7304c15a2bc497eaa0e3b217471c

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
 console.log('%c HI', 'color: firebrick')
+
+document.addEventListener('DOMContentLoaded', () => {
+    images()
+
+    function images(){
+        debugger
+        return fetch('https://dog.ceo/api/breeds/image/random/4')
+            .then(resp => resp.json())
+            .then(json => console.log(results))
+    }
+
+    function dogBreed(){
+        return fetch('https://dog.ceo/api/breeds/list/all')
+    }
+})
